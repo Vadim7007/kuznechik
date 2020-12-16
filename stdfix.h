d420a37d8904f025d1ce7c9c7c473774128ffefa
Vadim7007 <62574814+Vadim7007@users.noreply.github.com>
2020-12-16 18:09:29 +0300

    Add files via upload

--- stdfix.h
+++ stdfix.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2007-2017 Free Software Foundation, Inc.
+﻿/* Copyright (C) 2007-2017 Free Software Foundation, Inc.
 This file is part of GCC.
 GCC is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
